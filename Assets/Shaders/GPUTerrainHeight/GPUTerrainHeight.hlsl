897ddbb4fb046fef6e661dce0f92a2d7989888c1
Top61 <yao.liu@unity3d.com>
2021-07-27 00:01:00 +0800

    DownSample Mesh

--- Assets/Shaders/GPUTerrainHeight/GPUTerrainHeight.hlsl
+++ Assets/Shaders/GPUTerrainHeight/GPUTerrainHeight.hlsl
@@ -2,25 +2,7 @@
 
 float TerrainHeight(float x, float z)
 {
-    int biome0 = GetBiome(x,z);
-    int biome1 = GetBiome(x+1.0f,z);
-    int biome2 = GetBiome(x,z+1.0f);
-    int biome3 = GetBiome(x+1.0f,z+1.0f);
-
-    [branch]
-    if(biome0 == biome1 && biome1 == biome2 && biome2==biome3)
-    {
-        return GetBiomeHeight(biome0,x,z);
-    }
-    else
-    {
-        float biomeHeight0 = GetBiomeHeight(biome0,x,z);
-        float biomeHeight1 = GetBiomeHeight(biome1,x+1.0f,z);
-        float biomeHeight2 = GetBiomeHeight(biome2,x,z+1.0f);
-        float biomeHeight3 = GetBiomeHeight(biome3,x+1.0f,z+1.0f);
-        
-        return (biomeHeight0+biomeHeight1+biomeHeight2+biomeHeight3)/4;
-    }
+    return GetBiomeHeight(GetBiome(x,z),x,z);
 }
 
 void TerrainHeight_float(float x, float z, out float height)
