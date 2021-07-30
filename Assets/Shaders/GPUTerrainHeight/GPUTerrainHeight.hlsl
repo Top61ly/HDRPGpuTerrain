976796443b634d20a3d673c2998ac0a328a077e7
Top61 <yao.liu@unity3d.com>
2021-07-30 23:07:27 +0800

    Add Smooth Terrain Height

--- Assets/Shaders/GPUTerrainHeight/GPUTerrainHeight.hlsl
+++ Assets/Shaders/GPUTerrainHeight/GPUTerrainHeight.hlsl
@@ -1,5 +1,51 @@
 #include "./TerrainBiomeHeight.hlsl"
 
+// 3  x  2
+// y     y
+// 0  x  1
+float TerrainSmoothHeight(float2 vertexIndex, float x, float z, float patchSize, float2 patchPos)
+{
+    int biome0 = GetBiome(patchPos.x-patchSize/2, patchPos.y - patchSize/2);
+    int biome1 = GetBiome(patchPos.x+patchSize/2, patchPos.y - patchSize/2);
+    int biome2 = GetBiome(patchPos.x+patchSize/2, patchPos.y + patchSize/2);
+    int biome3 = GetBiome(patchPos.x-patchSize/2, patchPos.y + patchSize/2);
+    
+    float biomeHeight0 = GetBiomeHeight(biome0,x,z);
+    float biomeHeight1 = GetBiomeHeight(biome1,x,z);
+    float biomeHeight2 = GetBiomeHeight(biome2,x,z);
+    float biomeHeight3 = GetBiomeHeight(biome3,x,z);
+    
+    float t = smoothstep(0.0f,1.0f,vertexIndex.x/15.0f);
+    float t1 = smoothstep(0.0f,1.0f,vertexIndex.y/15.0f);
+
+    float a = lerp(biomeHeight0, biomeHeight1, t);
+    float b = lerp(biomeHeight3, biomeHeight2, t);
+    float c = lerp(a, b, t1);
+
+    return c;
+    // [branch]
+    // if(biome0 == biome1 && biome1 == biome2 && biome2==biome3)
+    // {
+    //     return GetBiomeHeight(GetBiome(x,z),x,z);
+    // }
+    // else
+    // {
+    //     float biomeHeight0 = GetBiomeHeight(biome0,x,z);
+    //     float biomeHeight1 = GetBiomeHeight(biome1,x,z);
+    //     float biomeHeight2 = GetBiomeHeight(biome2,x,z);
+    //     float biomeHeight3 = GetBiomeHeight(biome3,x,z);
+        
+    //     float t = smoothstep(0.0f,1.0f,vertexIndex.x/15);
+    //     float t1 = smoothstep(0.0f,1.0f,vertexIndex.y/15);
+
+    //     float a = lerp(biomeHeight0, biomeHeight1, t);
+    //     float b = lerp(biomeHeight3, biomeHeight2, t);
+    //     float c = lerp(a, b, t1);
+
+    //     return c;
+    // }
+}
+
 float TerrainHeight(float x, float z)
 {
     float scale = 4.0f;
--- Assets/Shaders/GPUTerrainHeight/TerrainVertexPosition.hlsl
+++ Assets/Shaders/GPUTerrainHeight/TerrainVertexPosition.hlsl
@@ -3,16 +3,25 @@
 
 StructuredBuffer<RenderPatch> renderPatchList;
 
+void GetVertexID_float(float3 posOS, out uint2 vertexIndex)
+{
+    vertexIndex = floor((posOS.xz + 8*0.5 + 0.01)/0.5);
+}
+
 void GetTerrainVertex_float(float instanceID, float3 posOS, out float3 oposOS, out float4 color)
 {
     RenderPatch patch = renderPatchList[(int)instanceID];
     float2 patchPos = patch.position;
     uint curlod = patch.lod;
-
+    uint2 vertexIndex = floor((posOS.xz + 8 * 0.5 + 0.01) / 0.5);
+    float patchSize = 8*pow(2,curlod);
     oposOS = posOS*pow(2,curlod);
     oposOS += float3(patchPos.x,0,patchPos.y);
     
-    oposOS.y = TerrainHeight(oposOS.x, oposOS.z)*200.0f;
+    //int biome = GetBiome(oposOS.x, oposOS.z);
+    //oposOS.y = GetBiomeHeight(biome,oposOS.x, oposOS.z)*200.0f;
+
+    oposOS.y = TerrainSmoothHeight((float2)vertexIndex, oposOS.x, oposOS.z, patchSize, patchPos)*200.0f;
     
     color = GetBlendMask(oposOS.x, oposOS.z);
 }
